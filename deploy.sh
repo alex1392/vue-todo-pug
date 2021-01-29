e7a7f00a3eb376b76b6b800c883762038560b9e3
Alex Chuang <1392alex1392@gmail.com>
2021-01-29 22:23:08 +0000

    push dist

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:alex1392/vue-todo-pug.git master:gh-pages
+git push -f git@github.com:alex1392/vue-todo-pug-dist.git master:gh-pages
 
 cd -
